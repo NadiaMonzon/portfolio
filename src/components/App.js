c573ccba01ca9ac7630928cc43c1888b08d5419a
NadiaMonzon <nadiamonque99@gmail.com>
2022-08-10 19:00:57 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='App--header'>
+
+      </header>
+
+
+      <main className='App--main'>
+
+      </main>
+
+
+      <footer className='App--footer'>
+
+      </footer>
+      
+    </div>
+  );
+}
+
+export default App;
