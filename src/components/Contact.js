987242df31838603af352aee4e528e97ae8278d9
NadiaMonzon <nadiamonque99@gmail.com>
2022-10-12 19:27:14 +0100

    Add contact section and styles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,10 @@
 const Contact =()=>{
-    return(<section id='contact'>
-        <h3>Feel free to contact me</h3>
-        <h4>Github</h4>
-        <h4>LinkedIn</h4>
-        <h4>Twitter</h4>
+    return(
+    <section id='contact' className="contact">
+        <h3 className="contact__title">Feel free to contact me through:</h3>
+        <h4 className='contact__title--social'><a href='https://github.com/NadiaMonzon' target='blank' className="contact__link" alt='Take a look at my GitHub profile'>Github</a></h4>
+        <h4 className='contact__title--social'><a href='https://www.linkedin.com/in/nadiamonzon/' className="contact__link" alt='Take a look at my LinkedIn profile'>LinkedIn</a></h4>
+        <h4 className='contact__title--social'><a href='mailto:nadiamonque99@gmail.com' className="contact__link">Mail</a></h4>
     </section>);
 }
 
